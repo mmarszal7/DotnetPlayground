74432b8b592ea785eb76fe2b93fa565867ba474d
mmarszal7 <mati.m7@o2.pl>
2017-09-28 20:30:00 +0100

    Config file and new class

--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,6 +8,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace WpfClient
 {
@@ -19,16 +22,18 @@ namespace WpfClient
     public partial class MainWindow : Window
     {
         private static Random rand = new Random();
-        private readonly int port;
-        private readonly string address = "127.0.0.1";
+        public UserData User { get; set; } = new UserData();
         private UdpClient client;
 
         public MainWindow()
         {
             InitializeComponent();
-            port = rand.Next(32100, 32200);
-            client = new UdpClient(new IPEndPoint(IPAddress.Parse(address), port));
-            MyAddress.Text = String.Format("My address: {0}:{1}", address, port);
+            GetUserData();
+            client = new UdpClient(new IPEndPoint(IPAddress.Parse(User.Address), User.Port));
+            txtMyAddress.Text = String.Format("My address: {0}:{1}", User.Address, User.Port);
+            txtName.Text = User.Name;
+            txtPort.Text = User.Port.ToString();
+            txtAddress.Text = User.Address;
 
             Task.Factory.StartNew(async () =>
             {
@@ -55,26 +60,42 @@ namespace WpfClient
             });
         }
 
-        private void txtBox_GotFocus(object sender, RoutedEventArgs e)
+        private void txtGotFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
             tb.Text = string.Empty;
-            tb.GotFocus -= txtBox_GotFocus;
+            tb.GotFocus -= txtGotFocus;
         }
 
         private void Send(object sender, RoutedEventArgs e)
         {
             try
             {
-                var datagram = Encoding.ASCII.GetBytes(String.Format("{0}: {1}", UserNameBox.Text, MessageBox.Text));
-                client.Send(datagram, datagram.Length, new IPEndPoint(IPAddress.Parse(Address.Text), Int32.Parse(Port.Text)));
-                MessageWindow.Text += String.Format("{0}: {1}", UserNameBox.Text, MessageBox.Text) + "\n";
+                var datagram = Encoding.ASCII.GetBytes(String.Format("{0}: {1}", txtName.Text, txtMessage.Text));
+                client.Send(datagram, datagram.Length, new IPEndPoint(IPAddress.Parse(txtAddress.Text), Int32.Parse(txtPort.Text)));
+                MessageWindow.Text += String.Format("{0}: {1}", txtName.Text, txtMessage.Text) + "\n";
                 Scroll.ScrollToBottom();
             }
             catch
             {
                 MessageWindow.Text += "Wrong address!\n";
                 Scroll.ScrollToBottom();
+            }
+        }
+
+        private void GetUserData()
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(UserData));
+                using (var reader = new StreamReader("user.xml"))
+                {
+                    User = (UserData)serializer.Deserialize(reader);
+                }
+            }
+            catch(Exception e)
+            {
+                Logger.Instance.Error("XML deserialization error - " + e);
             }
         }
     }
---
+++ WpfClient/UserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WpfClient
+{
+    [Serializable()]
+    public class UserData
+    {
+        [System.Xml.Serialization.XmlElement("Address")]
+        public string Address { get; set; } = "127.0.0.1";
+        [System.Xml.Serialization.XmlElement("Port")]
+        public int Port { get; set; } = 32100;
+        [System.Xml.Serialization.XmlElement("Name")]
+        public string Name { get; set; } = "Name";
+    }
+}
